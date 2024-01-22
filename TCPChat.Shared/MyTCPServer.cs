22f3d450571394a8e403175192c69b6043aac724
Maks <maksim.west00@gmail.com>
2024-01-22 23:24:55 +0300

    tcp chat

---
+++ TCPChat.Shared/MyTCPServer.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using TCPChat.Shared.ViewModels;
+
+namespace TCPChat.Shared
+{
+    public class MyTCPServer
+    {
+        public TcpListener TcpListener;
+        public MyTCPClient TcpClient;
+        public MainWindowViewModel MainWindowViewModel;
+        public MyTCPServer(MainWindowViewModel mainWindowViewModel)
+        {
+            this.TcpListener = new TcpListener(IPAddress.Any, 8888);
+            this.MainWindowViewModel = mainWindowViewModel;
+        }
+        public async Task ListenAsync()
+        {
+            try
+            {
+                this.TcpListener.Start();
+                this.MainWindowViewModel.ChatData = "[Сообщение на сервере] Сервер запущен.Ожидание подключений...";
+                while (true)
+                {
+                    TcpClient tcpClient = await this.TcpListener.AcceptTcpClientAsync();
+                    this.TcpClient = new MyTCPClient(tcpClient, this);
+                    this.TcpClient.Reader = new StreamReader(this.TcpClient.TcpClient.GetStream());
+                    this.TcpClient.Writer = new StreamWriter(this.TcpClient.TcpClient.GetStream());
+                    this.MainWindowViewModel.ChatData = "[Сообщение на сервере] Клиент подключен";
+                    await this.TcpClient.SendMessageAsync("Вы подключены");
+                    await Task.Run(this.TcpClient.ReceiveMessageAsync);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                DisconnectClient();
+            }
+        }
+        public async Task DisconnectClient()
+        {
+            if(this.TcpClient != null)
+            {
+                await this.TcpClient.SendMessageAsync("Сервер отключен");
+                this.TcpClient.CloseConnection();
+                this.MainWindowViewModel.ChatData = "[Сообщение на сервере] Клиент отключен";
+            }       
+        }
+        public async void StopServer()
+        {
+            await this.DisconnectClient();
+            this.TcpListener.Stop();
+            this.MainWindowViewModel.ChatData = "[Сообщение на сервере] Сервер отключен";
+        }
+    }
+}
---
+++ TCPChat.Shared/RelayCommand.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows.Input;
+
+namespace TCPChat.Shared
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+        private bool _isExecuting;
+        private EventHandler _canExecuteChanged;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            { 
+                _canExecuteChanged += value; 
+            }
+            remove 
+            { 
+                _canExecuteChanged -= value; 
+            }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return !_isExecuting && (_canExecute == null || _canExecute(parameter));
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_isExecuting)
+                return;
+
+            try
+            {
+                _isExecuting = true;
+                _canExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+                _execute(parameter);
+            }
+            finally
+            {
+                _isExecuting = false;
+                _canExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
