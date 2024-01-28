f9d05c3c1b4d0679fe8dd5b1c6d77e67c4a7cfe2
Maks <maksim.west00@gmail.com>
2024-01-28 17:29:30 +0300

    tcpchat avalonia

---
+++ TCPChat.Avalonia/App.axaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using TCPChat.Avalonia.Views;
+using TCPChat.Shared.ViewModels;
+
+namespace TCPChat.Avalonia
+{
+    public partial class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel()
+                };
+            }
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
+
